2acf78531cedcf873722f6b4a95fab6d834b8a8d
Winnie Chou <winnie.chou.mb@gmail.com>
2021-04-08 19:54:15 -0500

    completed build of Markov Machine

--- markov.js
+++ markov.js
@@ -6,8 +6,8 @@ class MarkovMachine {
   /** build markov machine; read in text.*/
 
   constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    // MORE CODE HERE
+    this.words = text.split(/[ \r\n]+/); // ["the", "cat", "in", "the", "hat"]
+    this.chains = this.makeChains(this.words);
   }
 
   /** set markov chains:
@@ -15,14 +15,43 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // MORE CODE HERE
+  makeChains(words) {
+    let chains = {};
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      const nextWord = words[i + 1] ? words[i + 1] : null;
+      if (!(word in chains)) {
+        chains[word] = [];
+      }
+      chains[word].push(nextWord);
+    }
+
+    return chains;
   }
 
 
   /** return random text from chains */
 
   getText(numWords = 100) {
-    // MORE CODE HERE
+    let randomIdx = Math.floor(Math.random() * this.words.length);
+    let word = this.words[randomIdx];
+    let text = word;
+    let count = 1;
+
+    while (count < numWords){
+      let nextWordOptions = this.chains[word];
+      randomIdx = Math.floor(Math.random() * nextWordOptions.length);
+      word = nextWordOptions[randomIdx];
+      if (word === null) break;
+      text += ' '+word;
+      count++;
+    }
+
+    return text;
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
